6abcfcaee3f5e5663d69b8c3e78f980839498708
hangsuu <ohhangsu@naver.com>
2024-01-04 22:07:00 +0900

    기본 통신 준비

--- src/main/java/com/example/insiderback/member/controller/MemberController.java
+++ src/main/java/com/example/insiderback/member/controller/MemberController.java
@@ -2,11 +2,13 @@ package com.example.insiderback.member.controller;
 
 import com.example.insiderback.member.model.MemberVO;
 import com.example.insiderback.member.service.LoginService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController("/member")
+@Slf4j
+@RestController
+@RequestMapping("/member")
 public class MemberController {
     @Autowired
     LoginService loginService;
@@ -16,14 +18,20 @@ public class MemberController {
         return loginService.getUser(id);
     }
     @PostMapping("/setUser")
-    public String setUser(MemberVO vo) {
+    public void setUser(@RequestBody MemberVO vo) {
+        log.info("전송 = {}", vo);
         loginService.setUser(vo);
-        return "입력 성공";
+        log.info("입력 성공 = {}", loginService.getUser(vo.getId()));
     }
 
     @PostMapping("/delUser")
     public String delUser(String id) {
         loginService.delUser(id);
         return "삭제 성공";
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "성공";
     }
 }
--- src/main/java/com/example/insiderback/member/model/MemberVO.java
+++ src/main/java/com/example/insiderback/member/model/MemberVO.java
@@ -1,8 +1,11 @@
 package com.example.insiderback.member.model;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 public class MemberVO {
     private String id;
     private String password;
