e46d18acc23b96925bdbcd7f0c88ed63756e1c24
hangsuu <ohhangsu@naver.com>
2024-01-04 20:14:32 +0900

    Initial commit

---
+++ src/main/java/com/example/insiderback/InsiderBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.insiderback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsiderBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsiderBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/insiderback/InsiderBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.insiderback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsiderBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
