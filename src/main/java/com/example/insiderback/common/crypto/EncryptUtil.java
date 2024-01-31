866dc1185f9a2444f743f4a010dab9ff68078b93
hangsuu <ohhangsu@naver.com>
2024-01-31 19:44:53 +0900

    EncryptUtil 암호화 메서드 생성

--- src/main/java/com/example/insiderback/common/crypto/EncryptUtil.java
+++ src/main/java/com/example/insiderback/common/crypto/EncryptUtil.java
@@ -3,12 +3,16 @@ package com.example.insiderback.common.crypto;
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 
 public class EncryptUtil {
     private static String alg = "AES/CBC/PKCS5Padding";
     private static String key = "a00aa00000aaaaaaafdfdfdf98989889";
     public static byte[] ivBytes = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+
+    // 양뱡함 암호화
     public String encrypt(String value) throws Exception{
         try {
             Cipher cipher = Cipher.getInstance(alg);
@@ -23,7 +27,34 @@ public class EncryptUtil {
         }
     }
 
-    public String decrypt(String value) {
-        return "**";
+    public String decrypt(String value) throws Exception {
+        try {
+            Cipher cipher = Cipher.getInstance(alg);
+            SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), "AES");
+            IvParameterSpec ivParameterSpec = new IvParameterSpec(ivBytes);
+            cipher.init(Cipher.DECRYPT_MODE, keySpec, ivParameterSpec);
+
+            byte[] encrypted = cipher.doFinal(value.getBytes("UTF-8"));
+            return Base64.getEncoder().encodeToString(encrypted);
+        } catch (NullPointerException e) {
+            return null;
+        }
+    }
+
+    // 단방향 암호화
+    public static String getHashEncryptSHA256 (String text) {
+        try {
+            // SHA-256 해시 계산
+            MessageDigest sha256 = MessageDigest.getInstance("SHA-256");
+            byte[] hashBytes = sha256.digest(text.getBytes());
+
+            // Base64로 인코딩
+            String base64Encoded = Base64.getEncoder().encodeToString(hashBytes);
+
+            return base64Encoded;
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
