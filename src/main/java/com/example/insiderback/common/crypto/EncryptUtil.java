5ce47b2c2c23d54e60ee1d9d52dc234e04c6d0a2
hangsuu <ohhangsu@naver.com>
2024-01-31 18:18:05 +0900

    EncryptUtil 암호화 메서드 생성

--- src/main/java/com/example/insiderback/common/crypto/EncryptUtil.java
+++ src/main/java/com/example/insiderback/common/crypto/EncryptUtil.java
@@ -1,8 +1,26 @@
 package com.example.insiderback.common.crypto;
 
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
 public class EncryptUtil {
-    public String encrypt(String value) {
-        return "**";
+    private static String alg = "AES/CBC/PKCS5Padding";
+    private static String key = "a00aa00000aaaaaaafdfdfdf98989889";
+    public static byte[] ivBytes = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+    public String encrypt(String value) throws Exception{
+        try {
+            Cipher cipher = Cipher.getInstance(alg);
+            SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), "AES");
+            IvParameterSpec ivParameterSpec = new IvParameterSpec(ivBytes);
+            cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivParameterSpec);
+
+            byte[] encrypted = cipher.doFinal(value.getBytes("UTF-8"));
+            return Base64.getEncoder().encodeToString(encrypted);
+        } catch (NullPointerException e) {
+            return null;
+        }
     }
 
     public String decrypt(String value) {
