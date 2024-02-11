5ee8577ecf93a9800e331fdae8dfa7a5f5636c7a
hangsuu <ohhangsu@naver.com>
2024-02-11 19:42:57 +0900

    파일 입력

--- src/main/java/com/example/insiderback/member/controller/MemberController.java
+++ src/main/java/com/example/insiderback/member/controller/MemberController.java
@@ -76,12 +76,10 @@ public class MemberController {
         if(!attach.isEmpty()) {	//파일이 있을 경우
             //번호생성
             int attachmentNo = attachmentMapper.selectAttachmentNo();
-
             File dir = new File("D:/upload/insider-back");	//파일 저장 위치
             dir.mkdirs(); 		//폴더 생성 명령
             File target = new File(dir, String.valueOf(attachmentNo));		//파일명은 int로 안들어감
             attach.transferTo(target);
-
             //DB 저장
             AttachmentVO attachmentVO = AttachmentVO.builder()
                     .attachmentNo(attachmentNo)
